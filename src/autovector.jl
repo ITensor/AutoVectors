d359255bd9923a817f2191f0cfafe75cd9da90a5
Matthew Fishman <mfishman@flatironinstitute.org>
2019-06-25 11:56:36 -0400

    Some formatting improvements. Add AutoVector() = AutoVector(0.0)

--- src/autovector.jl
+++ src/autovector.jl
@@ -1,33 +1,56 @@
 
 mutable struct AutoVector{T}  			#   <: AbstractVector{T}   doesn't work
-	mini::Int64
-	maxi::Int64
-	miniloc::Int64		# the location/index of mini-1 in dat
-	def::T
-	#dat::AbstractVector{T}
-	dat::Vector{T}
-    function AutoVector{T}(def::T,mini::Integer,maxi::Integer,miniloc::Integer,
-			   				dat::Vector{T}) where T
-	length(dat) < maxi-mini+1 && error("dat too small in AutoVector constructor")
-	new(mini,maxi,miniloc,deepcopy(def),dat)
-    end
+  mini::Int64
+  maxi::Int64
+  miniloc::Int64		# the location/index of mini-1 in dat
+  def::T
+  #dat::AbstractVector{T}
+  dat::Vector{T}
+  function AutoVector{T}(def::T,mini::Integer,
+                         maxi::Integer,miniloc::Integer,
+                         dat::Vector{T}) where T
+    length(dat) < maxi-mini+1 && error("dat too small in AutoVector constructor")
+    new(mini,maxi,miniloc,deepcopy(def),dat)
+  end
 end
 
-AutoVector(def::T,mini::Integer,maxi::Integer,miniloc::Integer,dat::Vector{T}) where T = 
-	    AutoVector{T}(def,mini,maxi,miniloc,dat)
-AutoVector(def::T,mini::Integer,maxi::Integer,miniloc::Integer) where T = 
-    AutoVector(def,mini,maxi,miniloc,T[deepcopy(def) for j=1:maxi-mini+1])
+AutoVector(def::T,
+           mini::Integer,
+           maxi::Integer,
+           miniloc::Integer,
+           dat::Vector{T}) where T = 
+  AutoVector{T}(def,mini,maxi,miniloc,dat)
+AutoVector(def::T,
+           mini::Integer,
+           maxi::Integer,
+           miniloc::Integer) where T = 
+  AutoVector(def,mini,maxi,miniloc,T[deepcopy(def) for j=1:maxi-mini+1])
+
+"""
+AutoVector(x::T)
+
+Create an AutoVector of length 0 with a constant value of x.
+
+For example: v = AutoVector(0.0)
+"""
 AutoVector(def::T) where T = AutoVector(deepcopy(def),1,0,0)
 
+"""
+AutoVector(x::T)
+
+Create an AutoVector of length 0 with a constant value of 0.0.
+
+For example: v = AutoVector()
+"""
+AutoVector() = AutoVector(0.0)
+
 function AutoVector(f::Function,mini::Integer,maxi::Integer)
-    dat = [f(i) for i=mini:maxi]
-    AutoVector(zero(typeof(dat[1])),mini,maxi,0,dat)
+  dat = [f(i) for i=mini:maxi]
+  AutoVector(zero(typeof(dat[1])),mini,maxi,0,dat)
 end
 AutoVector(v::Vector{T},mini::Integer,maxi::Integer) where T =
     AutoVector(zero(typeof(v[1])),mini,maxi,0,v)
 AutoVector(v::Vector{T}) where T = AutoVector(zero(typeof(v[1])),1,length(v),0,v)
-
-# Usage:  v = AutoVector(0.0)
 
 mini(v::AutoVector) = v.mini
 maxi(v::AutoVector) = v.maxi
@@ -111,23 +134,23 @@ end
 
 import Base.*
 function *(f::Real,v::AutoVector)
-    newv = deepcopy(v)
-    newv.dat *= f
-    newv
+  newv = deepcopy(v)
+  newv.dat *= f
+  newv
 end
 function *(v::AutoVector,f::Real) f*v end
 
 function *(coef::Vector{Float64},v::Vector{AutoVector{Float64}})
-    n = length(v)
-    mi = minimum([mini(v[i]) for i=1:n])
-    ma = maximum([maxi(v[i]) for i=1:n])
-    len = ma-mi+1
-    res = zeros(len)
-    for i=1:n
-	ra = arange(v[i]) .+ (1-mi)
-	res[ra] += coef[i] * avvec(v[i])
-    end
-    AutoVector(res,mi,ma)
+  n = length(v)
+  mi = minimum([mini(v[i]) for i=1:n])
+  ma = maximum([maxi(v[i]) for i=1:n])
+  len = ma-mi+1
+  res = zeros(len)
+  for i=1:n
+    ra = arange(v[i]) .+ (1-mi)
+    res[ra] += coef[i] * avvec(v[i])
+  end
+  AutoVector(res,mi,ma)
 end
 
 import Base.broadcast
@@ -136,18 +159,18 @@ import Base.broadcast
 #import Base.(.*)
 #function .*(x::AutoVector,y::AutoVector)
 function broadcast(::typeof(*),x::AutoVector,y::AutoVector)
-    a = max(mini(x),mini(y))
-    b = min(maxi(x),maxi(y))
-    @inbounds xy = Float64[fast(x,i)*fast(y,i) for i=a:b]
-    newv = AutoVector(xy,a,b)
-    #=
-    oldv = AutoVector(0.0)
-    for i=a:b
-	oldv[i] = x[i] * y[i]
-    end
-    @show vecnorm((oldv-newv).dat)
-    =#
-    newv
+  a = max(mini(x),mini(y))
+  b = min(maxi(x),maxi(y))
+  @inbounds xy = Float64[fast(x,i)*fast(y,i) for i=a:b]
+  newv = AutoVector(xy,a,b)
+  #=
+  oldv = AutoVector(0.0)
+  for i=a:b
+    oldv[i] = x[i] * y[i]
+  end
+  @show vecnorm((oldv-newv).dat)
+  =#
+  newv
 end
 
 function avdot(x::AutoVector,y::AutoVector)
@@ -217,33 +240,37 @@ end
 
 #  Same as avdot(convolve(u,g),v)
 function avtripconv(u::AutoVector,g::AutoVector,v::AutoVector)
-    ua,ub = mini(u),maxi(u)
-    ga,gb = mini(g),maxi(g)
-    va,vb = mini(v),maxi(v)
-    ub+gb < va && return 0.0
-    ua+ga > vb && return 0.0
-    dotrip(u.dat,g.dat,v.dat,ua,ub,ga,va,gb,vb,u.mini-u.miniloc-1,g.mini-g.miniloc-1,v.mini-v.miniloc-1)
+  ua,ub = mini(u),maxi(u)
+  ga,gb = mini(g),maxi(g)
+  va,vb = mini(v),maxi(v)
+  ub+gb < va && return 0.0
+  ua+ga > vb && return 0.0
+  dotrip(u.dat,g.dat,v.dat,
+         ua,ub,ga,va,gb,vb,
+         u.mini-u.miniloc-1,
+         g.mini-g.miniloc-1,
+         v.mini-v.miniloc-1)
 end
 
 function tripconv(u::Vector{Float64},g::Vector{Float64},v::Vector{Float64})
-    k,m,n = length(u),length(g),length(v)
-    res = 0.0
-    for i=1:min(k,n), j=1:min(m,n-i+1)
-	@inbounds res += u[i] * g[j] * v[i+j-1]
-    end
-    res
+  k,m,n = length(u),length(g),length(v)
+  res = 0.0
+  for i=1:min(k,n), j=1:min(m,n-i+1)
+    @inbounds res += u[i] * g[j] * v[i+j-1]
+  end
+  res
 end
 
 function convolvecheck(x::Vector{Float64},y::Vector{Float64})
-    mx = length(x)
-    my = length(y)
-    res = zeros(Float64,mx+my-1)
-    for j = 1:mx
-@inbounds	for k = 1:my
+  mx = length(x)
+  my = length(y)
+  res = zeros(Float64,mx+my-1)
+  for j = 1:mx
+    @inbounds	for k = 1:my
 	    res[j+k-1] += x[j] * y[k]
-	end
-    end
-    res
+	  end
+  end
+  res
 end
 
 function myconv(u::Vector{Float64},v::Vector{Float64})
@@ -264,43 +291,43 @@ function myconv(u::Vector{Float64},v::Vector{Float64})
 end
 
 function convolve(x::AutoVector,y::AutoVector,cut=1.0e-14)		# use absolute cutoff
-    res = AutoVector(x.def)
-#println(maxi(x)-mini(x),"  ",maxi(y)-mini(y))
-    if typeof(x.def) == Float64
-	mix = mini(x)
-	mx = maxi(x)
-	mx-mix < 0 && return res
-	#xx = Float64[fast(x,i+mix-1) for i=1:mx-mix+1]
-	xx = avvec(x)
-	miy = mini(y)
-	my = maxi(y)
-	my-miy < 0 && return res
-	#yy = Float64[fast(y,i+miy-1) for i=1:my-miy+1]
-	yy = avvec(y)
-#	@time vres2 = convolvecheck(xx,yy)
-	vres = myconv(xx,yy)
-#	@show vecnorm(vres-vres2)
-	mir = mix+miy
-	minj = 1
-	for j = 1:length(vres)
-	    abs(vres[j]) > cut && (minj = j; break)
-	end
-	maxj = length(vres)
-	for j = length(vres):-1:1
-	    abs(vres[j]) > cut && (maxj = j; break)
-	end
-	res.dat = vres[minj:maxj]
-	res.mini=minj+mir-1
-	res.maxi=maxj+mir-1
-	res.miniloc=0
-    else
-	for j = mini(x):maxi(x)
-	    for k = mini(y):maxi(y)
-		res[j+k] += x[j] * y[k]
-	    end
-	end
+  res = AutoVector(x.def)
+  #println(maxi(x)-mini(x),"  ",maxi(y)-mini(y))
+  if typeof(x.def) == Float64
+    mix = mini(x)
+    mx = maxi(x)
+    mx-mix < 0 && return res
+    #xx = Float64[fast(x,i+mix-1) for i=1:mx-mix+1]
+    xx = avvec(x)
+    miy = mini(y)
+    my = maxi(y)
+    my-miy < 0 && return res
+    #yy = Float64[fast(y,i+miy-1) for i=1:my-miy+1]
+    yy = avvec(y)
+    #	@time vres2 = convolvecheck(xx,yy)
+    vres = myconv(xx,yy)
+    #	@show vecnorm(vres-vres2)
+    mir = mix+miy
+    minj = 1
+    for j = 1:length(vres)
+      abs(vres[j]) > cut && (minj = j; break)
     end
-    res
+    maxj = length(vres)
+    for j = length(vres):-1:1
+      abs(vres[j]) > cut && (maxj = j; break)
+    end
+    res.dat = vres[minj:maxj]
+    res.mini=minj+mir-1
+    res.maxi=maxj+mir-1
+    res.miniloc=0
+  else
+    for j = mini(x):maxi(x)
+      for k = mini(y):maxi(y)
+        res[j+k] += x[j] * y[k]
+      end
+    end
+  end
+  res
 end
 
 function makeautotake(v::Vector{Float64},offset::Integer)		# take v as the data; very fast
@@ -370,33 +397,33 @@ function shrink!(x::AutoVector,cut)
     x.mini = newmin
 end
 function shrinkold!(x::AutoVector,cut)
-    newmin = maxi(x)+1
-    for i=arange(x)
-	if(abs(fast(x,i)) > cut)
+  newmin = maxi(x)+1
+  for i=arange(x)
+    if(abs(fast(x,i)) > cut)
 	    newmin = i
 	    break
-	end
     end
-    if newmin == maxi(x)+1 
-	clear!(x)
-	return
-    end
-    newmax = newmin-1
-    for i=maxi(x):-1:newmin
-	if(abs(fast(x,i)) > cut)
+  end
+  if newmin == maxi(x)+1 
+	  clear!(x)
+	  return
+  end
+  newmax = newmin-1
+  for i=maxi(x):-1:newmin
+	  if(abs(fast(x,i)) > cut)
 	    newmax = i
 	    break
-	end
     end
-    x.maxi = newmax
-    x.miniloc += newmin - x.mini
-    x.mini = newmin
+  end
+  x.maxi = newmax
+  x.miniloc += newmin - x.mini
+  x.mini = newmin
 end
 
 function eigsym(Marg)
-    M = copy(Marg)
-    symmetrize!(M)
-    F = eigen(M)
-    F.values,F.vectors
+  M = copy(Marg)
+  symmetrize!(M)
+  F = eigen(M)
+  F.values,F.vectors
 end
 
